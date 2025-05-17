f5fddc12641bd09c062063c58e6bb6a889c3f96d
manjur0 <sfmanjur21@gmail.com>
2025-05-17 22:14:38 +0600

    Add Hero component and SVG icons for GitHub and Figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
 import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 };
---
+++ src/Components/SVG/Figma.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Figma = () => {
+  return (
+    <a
+      href="#"
+      className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center
+               text-gray-900 border border-gray-200 rounded-lg sm:w-auto
+               hover:bg-gray-100 focus:ring-4 focus:ring-gray-100
+               dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+    >
+      <svg
+        className="w-4 h-4 mr-2 text-gray-500 dark:text-gray-400"
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 200 300"
+        width="1667"
+        height="2500"
+      >
+        <style type="text/css">
+          {`
+          .st0 { fill: #0acf83; }
+          .st1 { fill: #a259ff; }
+          .st2 { fill: #f24e1e; }
+          .st3 { fill: #ff7262; }
+          .st4 { fill: #1abcfe; }
+        `}
+        </style>
+        <title>Figma.logo</title>
+        <desc>Created using Figma</desc>
+
+        <path
+          id="path0_fill"
+          className="st0"
+          d="M50 300c27.6 0 50-22.4 50-50v-50H50c-27.6 0-50 22.4-50 50s22.4 50 50 50z"
+        />
+        <path
+          id="path1_fill"
+          className="st1"
+          d="M0 150c0-27.6 22.4-50 50-50h50v100H50c-27.6 0-50-22.4-50-50z"
+        />
+        <path
+          id="path1_fill_1_"
+          className="st2"
+          d="M0 50C0 22.4 22.4 0 50 0h50v100H50C22.4 100 0 77.6 0 50z"
+        />
+        <path
+          id="path2_fill"
+          className="st3"
+          d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50V0z"
+        />
+        <path
+          id="path3_fill"
+          className="st4"
+          d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z"
+        />
+      </svg>
+      Get Figma file
+    </a>
+  );
+};
+
+export default Figma;
