6b0a1aac6c21d271e7609b05253a686e556afd84
manjur0 <sfmanjur21@gmail.com>
2025-05-18 21:11:12 +0600

    Add PricingSection and PricingHeaderContent components to display pricing plans

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import CompanysLogo from "./Components/CompanysLogo";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import PricingSection from "./Components/PricingSection";
 import ServiceSection from "./Components/ServiceSection";
 import TestimonialSection from "./Components/TestimonialSection";
 import TrustedSection from "./Components/TrustedSection";
@@ -14,6 +15,7 @@ const App = () => {
       <ServiceSection />
       <TrustedSection />
       <TestimonialSection />
+      <PricingSection />
     </div>
   );
 };
