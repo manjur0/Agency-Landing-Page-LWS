e633177900e16e16f7e92aa729dd0d0f8330ddab
manjur0 <sfmanjur21@gmail.com>
2025-05-18 21:52:47 +0600

    Add Footer and Trial components to enhance the application layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CompanysLogo from "./Components/CompanysLogo";
 import FAQSection from "./Components/FAQSection";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import PricingSection from "./Components/PricingSection";
 import ServiceSection from "./Components/ServiceSection";
 import TestimonialSection from "./Components/TestimonialSection";
+import Trial from "./Components/Trial";
 import TrustedSection from "./Components/TrustedSection";
 
 const App = () => {
@@ -18,6 +20,8 @@ const App = () => {
       <TestimonialSection />
       <PricingSection />
       <FAQSection />
+      <Trial />
+      <Footer />
     </div>
   );
 };
